1a5f457a4939fdd7a9d8e62d1db18c592bf9d16e
Claudia Lay <claudialay@claudias-mbp-2.home>
2020-09-13 13:14:31 -0500

    added write file functionality

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,135 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project? (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter a project name!');
+            return false;
+          }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project (Required)',
+        validate: descripInput => {
+          if (descripInput) {
+            return true;
+          } else {
+            console.log('Please enter a description!');
+            return false;
+          }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmInstall',
+        message: 'Would you like to add installation instructions?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide some information about yourself:',
+        when: ({ confirmInstall }) => confirmInstall
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use.',
+        validate: usageInput => {
+          if (usageInput) {
+            return true;
+          } else {
+            console.log('Please enter instructions!');
+            return false;
+          }
+        }
+    },
+    {
+        type: 'checkbox',
+        name: 'licenses',
+        message: 'What license do you have for your project?',
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software Liense 1.0']
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Provide guidelines for how other developers to contribute.'
+    },
+    {
+        type: 'confirm',
+        name: 'confirmTests',
+        message: 'Would you like to add test instructions?',
+        default: false
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide examples on how to run the tests.',
+        when: ({ confirmTests }) => confirmTests
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your github username?',
+        validate: userInput => {
+            if (userInput) {
+              return true;
+            } else {
+              console.log('Please enter your username!');
+              return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?',
+        validate: emailInput => {
+            if (emailInput) {
+              return true;
+            } else {
+              console.log('Please enter your email!');
+              return false;
+            }
+        }
+    } 
+    // 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile('README.md', data, function(err) {
+        if(err)
+        {
+            return console.log(err);
+        }
+        console.log("Done!");
+        console.log(data);
+
+    })
 }
 
 // function to initialize program
 function init() {
-
+    return inquirer.prompt(questions)
 }
 
 // function call to initialize program
-init();
+init()
+    .then(data => {
+        return generateMarkdown(data)
+        })
+    .then(readMe => {
+        return writeToFile(readMe);
+        })  
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,29 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  #Licenses
+
+
+  #Description
+  ${data.description}
+
+  #Table of Contents
+ 
+
+  #Installation
+  ${data.installation}
+
+  #Usage
+  ${data.usage}
+
+  #Contributing
+  ${data.contributing}
+
+  #Tests
+  ${data.tests}
+  
+  #Questions
+  
 
 `;
 }
